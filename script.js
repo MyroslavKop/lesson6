189ad6447466e92cd9f611491959ffc03d84f69e
MiroslavKop <kopytovmiroslav@gmail.com>
2022-08-03 17:01:09 +0300

    добавил файлы из дз №1

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Задача №1
+const user = prompt("Как тебя зовут?", "");
+alert(user);
+console.log(user);
+
+// Задача №2
+let x = 20,
+  y = 10;
+alert(x + y);
+alert(x - y);
+alert(x / y);
+alert(x * y);
+alert(x ** y);
